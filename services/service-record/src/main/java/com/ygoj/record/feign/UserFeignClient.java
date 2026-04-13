e40c9b8f401dec3799f46d0df73d060db58db51d
B-up-yige <1534493926@qq.com>
2026-04-13 13:32:35 +0800

    service-problem:修复题目集和比赛功能

--- services/service-record/src/main/java/com/ygoj/record/feign/UserFeignClient.java
+++ services/service-record/src/main/java/com/ygoj/record/feign/UserFeignClient.java
@@ -6,10 +6,8 @@ import com.ygoj.user.Userinfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "service-user")
-@RequestMapping("/user")
+@FeignClient(value = "service-user", path = "/user")
 public interface UserFeignClient {
 
     @GetMapping("/userinfo/{id}")
