7c2ddb945b77f09c8d424a47460d0553425f838e
B-up-yige <1534493926@qq.com>
2026-04-17 10:49:05 +0800

    完善权限

--- fileSystem/src/main/java/com/ygoj/filesystem/FileSystemMainApplication.java
+++ fileSystem/src/main/java/com/ygoj/filesystem/FileSystemMainApplication.java
@@ -3,9 +3,13 @@ package com.ygoj.filesystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ygoj.filesystem", "com.ygoj.common"})
 public class FileSystemMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(FileSystemMainApplication.class, args);
---
+++ fileSystem/src/main/java/com/ygoj/filesystem/config/FeignMultipartConfig.java
@@ -0,0 +1,18 @@
+package com.ygoj.filesystem.config;
+
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class FeignMultipartConfig {
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder();
+    }
+
+    @Bean
+    public feign.Logger.Level multipartLoggerLevel() {
+        return feign.Logger.Level.FULL;
+    }
+}
